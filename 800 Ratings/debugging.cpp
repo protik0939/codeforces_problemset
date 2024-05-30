4e9c4b4a8fb42d6835925678b1aa943a1444cf42
protik0939 <protik0939@gmail.com>
2024-05-30 18:58:42 +0600

    30-5-24 (2)

---
+++ 800 Ratings/debugging.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define ll long long
+/*
+n-o-b-o-d-y
+cares you here
+without your
+r-a-t-i-n-g-s
+ */
+ 
+/*
+j
+s
+i
+y
+u
+m
+*/
+#define pb push_back
+#define ff first
+#define ss second
+#define mp make_pair
+#define ios ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+using namespace std;
+
+int main(){
+    ll t;
+    cin >> t;
+    while(t--)
+    {
+    ll a, b;
+    cin >> a >> b;
+    cout << 2 * b << endl;
+    return 0;
+    }
+}
